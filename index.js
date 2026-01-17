a96e27f10633c6b4b1389ed9d7b798de4a7e74e0
Nihal <nihallaskar888@gmail.com>
2026-01-17 23:30:50 +0600

    some changes for security

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const express = require('express')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 
@@ -8,7 +9,7 @@ const port = process.env.PORT || 5000
 const cors = require("cors")
 app.use(cors())
 app.use(express.json())
-const uri = "mongodb+srv://Nihal:hdasjkfhadkshfdsa@wavetone.jpf1fel.mongodb.net/?appName=Wavetone";
+const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -35,9 +36,9 @@ async function run() {
 
         //   apis starts from here
 
-        app.get("/products",async (req, res) => {
-              const result = await productCollection.find().toArray()
-              res.send(result)
+        app.get("/products", async (req, res) => {
+            const result = await productCollection.find().toArray()
+            res.send(result)
         })
 
 
