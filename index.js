ca1a5e65d3c9d90d8f8d181fc5f6305dc7ca9675
Nihal <nihallaskar888@gmail.com>
2026-01-17 23:40:04 +0600

    some changes to solve errors

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
 const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
-
-
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require("cors")
 app.use(cors())
 app.use(express.json())
+
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri, {
     serverApi: {
